370f8df32d65d02a80e581bb4fa7dd0453da11f2
Johan Kjölhede <gigurra@gmail.com>
2023-01-29 20:06:47 +0100

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,7 @@ final log = Logger(appTitle);
 
 void main() {
   initLogging();
-  final themeState = AppThemeState();
-  final domainState = AppDomainState();
-  runApp(AppBootstrapper(themeState, domainState));
+  runApp(AppBootstrapper(AppThemeState(), AppDomainState()));
 }
 
 class AppBootstrapper extends StatelessWidget {
