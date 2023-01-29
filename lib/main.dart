a0fbbf81a410563348899885aebf9589692e009c
Johan Kjölhede <gigurra@gmail.com>
2023-01-29 21:04:46 +0100

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutteridoo/state_binding.dart';
 import 'package:logging/logging.dart';
@@ -129,8 +131,6 @@ AppBar createAppBar(BuildContext context, String title) {
   log.info("Created app bar");
   return AppBar(
     title: Text(title),
-    // backgroundColor: Theme.of(context).colorScheme.background,
-    //  foregroundColor: Theme.of(context).colorScheme.onBackground,
   );
 }
 
